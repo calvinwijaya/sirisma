6dd707326bc4b1d41163a21742a7d12ab92328dc
calvinwijaya <calvin.wijaya@mail.ugm.ac.id>
2026-04-21 15:42:40 +0700

    Update gas login

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const GAS_LOGIN = "https://script.google.com/macros/s/AKfycbySNOI42wFZPnrfMXNUqjzi-raFkOrmhSZ5sOiBonVsylB4cXyd67DE6KlHllGPJ_M/exec";
+const GAS_LOGIN = "https://script.google.com/macros/s/AKfycbxnPRiupDEcSswpEt_zLxAXxxd-bJL7DE8pJTGumkAblJfB8w6FguYsvsJXdaqC020K/exec";
 const GAS_DATABASE = "https://script.google.com/macros/s/AKfycbxjBGq7BJ0CfSkFMqzu41WjexwKf7XxBoL42Htw9OMPNglCUduvp22ouFgSepGGe_o/exec";
 const GAS_JURNALHUB = "https://script.google.com/macros/s/AKfycbxRhnDgszhYukjWQUUiU43BRJIRH5syr6j991hkwE_toHceNWk0MvIrKTKak5FMegpu1Q/exec";
 const GAS_MAHASISWA = "https://script.google.com/macros/s/AKfycbywanxoTB4sJStnJmDLlrjlaNVHiaBz8DPA_LCR8sMJqE84T4B4NgyOCcZ1kuzcs8kC/exec";
